bc1cdb3ca8801d691fe20327b4d3d8b9036fb947
Jonathan Bentley <jonconnorbentley@gmail.com>
2017-09-28 12:09:07 -0400

    Nav Bar fixed

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -5,11 +5,24 @@ export default class Nav extends Component {
   render() {
     return (
       <div className="nav">
-        <Link to="/">Home</Link>
-        <Link to="/timer">Contraction<br/>Timer</Link>
-        <Link to="/nbl">Newborn<br/>Log</Link>
-        <Link to="/momlog">Mom's<br/>Log</Link>
-        <Link to="/profile">Profile</Link>
+        <Link to="/">
+        <span className="navwords">Home</span>
+        </Link>
+        <Link to="/timer">
+        <span className="navwords">Contraction</span>
+        <span className="navwords">Timer</span>
+        </Link>
+        <Link to="/nbl">
+        <span className="navwords">Newborn</span>
+        <span className="navwords">Log</span>
+        </Link>
+        <Link to="/momlog">
+        <span className="navwords">Momma's</span>
+        <span className="navwords">Log</span>
+        </Link>
+        <Link to="/profile">
+        <span className="navwords">Profile</span>
+        </Link>
       </div>
     );
   }
