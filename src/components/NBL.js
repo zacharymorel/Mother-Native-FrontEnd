4a2ee3cab5c83c4491106598030734e65a0864c5
Jonathan Bentley <jonconnorbentley@gmail.com>
2017-09-28 14:17:58 -0400

    newborn log layout finished.

--- src/components/NBL.js
+++ src/components/NBL.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
 
 export default class NBL extends Component {
+  state = {
+    ifFeed: false,
+    ifSleep: false,
+    ifDiaper: false,
+    ifNotes: false
+  }
+
+  _feedClick = ()=> {
+
+  }
+
   render() {
     return (
       <div className="nbl">
-        <p>hey</p>
+        <button className="nblButton">Feeding</button>
+        <button className="nblButton">Sleep</button>
+        <button className="nblButton">Diaper</button>
+        <button className="nblButton">Notes</button>
       </div>
     );
   }
