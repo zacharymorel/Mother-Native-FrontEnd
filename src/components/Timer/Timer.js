941958b7a48edcc3c390e445eb0eb15fb3460387
Jonathan Bentley <jonconnorbentley@gmail.com>
2017-10-12 17:36:24 -0400

    added fetch for contraction log, still needs testing

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -1,48 +1,38 @@
 import React, { Component } from 'react';
+import axios from 'axios'
+import {getAccessToken} from '../../utils/AuthService'
 
 import TimerTop from './TimerTop'
 
 export default class Timer extends Component {
+  state = {
+    contractions: []
+  }
+
+  componentDidMount() {
+    let url = 'https://the-best-mom-app.herokuapp.com/api/mom/contraction'
+    axios.get(url, {headers: { Authorization: `Bearer ${getAccessToken()}`}})
+    .then(res=> {
+    console.log(res)
+    this.setState({contractions: [{duration:'long',clocktimerstampstop:'now'},{duration:'long',clocktimerstampstop:'now'}]})
+    })
+  }
+
   render() {
+    let contractionlis = this.state.contractions.map(contraction => {
+      return(
+        <li className="contraction">
+          <span>Contraction:</span>
+          <span className="timestamp">{contraction.clocktimerstampstop}</span>
+          <span className="duration">{contraction.duration}</span>
+        </li>
+      )
+    })
     return (
       <div className="timer">
         <TimerTop/>
         <ul className="laps">
-          <li className="contraction">
-            <span>Contraction:</span>
-            <span className="timestamp">##:##</span>
-            <span className="duration">(#:##)</span>
-          </li>
-          <li className="contraction">
-            <span>Contraction:</span>
-            <span className="timestamp">##:##</span>
-            <span className="duration">(#:##)</span>
-          </li>
-          <li className="contraction">
-            <span>Contraction:</span>
-            <span className="timestamp">##:##</span>
-            <span className="duration">(#:##)</span>
-          </li>
-          <li className="contraction">
-            <span>Contraction:</span>
-            <span className="timestamp">##:##</span>
-            <span className="duration">(#:##)</span>
-          </li>
-          <li className="contraction">
-            <span>Contraction:</span>
-            <span className="timestamp">##:##</span>
-            <span className="duration">(#:##)</span>
-          </li>
-          <li className="contraction">
-            <span>Contraction:</span>
-            <span className="timestamp">##:##</span>
-            <span className="duration">(#:##)</span>
-          </li>
-          <li className="contraction">
-            <span>Contraction:</span>
-            <span className="timestamp">##:##</span>
-            <span className="duration">(#:##)</span>
-          </li>
+          {contractionlis}
         </ul>
       </div>
     );
--- src/components/Timer/TimerTop.js
+++ src/components/Timer/TimerTop.js
@@ -10,56 +10,53 @@ export default class TimerTop extends Component {
     countDisplay: '0:0',
   }
 
-count = () => {
-    this.setState(current => {return ({counter: current.counter+=1})})
-    this.setState(current => {
-      let seconds = current.counter%60
-      let minutes = ((current.counter - seconds) / 60)
-      return ({countDisplay: `${minutes}:${seconds}`})
+  count = () => {
+      this.setState(current => {return ({counter: current.counter+=1})})
+      this.setState(current => {
+        let seconds = current.counter%60
+        let minutes = ((current.counter - seconds) / 60)
+        return ({countDisplay: `${minutes}:${seconds}`})
+      })
+    }
+
+  startCounting = () => {
+    console.log('Start!');
+    this.setState({counting: true})
+    this.timer = setInterval(this.count, 1000)
+  }
+
+  stopCounting = () => {
+    console.log('Stop!');
+    this.setState({counting: false})
+    clearInterval(this.timer)
+  }
+
+  stampandsend = () => {
+    let now = moment()._d
+    let url = 'https://the-best-mom-app.herokuapp.com/api/mom/contraction'
+    console.log(now)
+    console.log(this.state.counter)
+    axios({
+      method: 'post',
+      url: 'https://the-best-mom-app.herokuapp.com/api/mom/contraction',
+      headers: { Authorization: `Bearer ${getAccessToken()}`},
+      data: {
+        duration: this.state.counter,
+        clocktimerstampstop: `${moment().format('YYYY-MM-DD HH:mm:ss')}`
+      }
     })
   }
 
-startCounting = () => {
-  console.log('Start!');
-  this.setState({counting: true})
-  this.timer = setInterval(this.count, 1000)
-}
 
-stopCounting = () => {
-  console.log('Stop!');
-  this.setState({counting: false})
-  clearInterval(this.timer)
-}
-
-stampandsend = () => {
-  let now = moment()._d
-  let url = 'https://the-best-mom-app.herokuapp.com/api/mom/contraction'
-  console.log(now)
-  console.log(this.state.counter)
-  console.log('try3')
-  axios({
-    method: 'post',
-    url: 'https://the-best-mom-app.herokuapp.com/api/mom/contraction',
-    headers: { Authorization: `Bearer ${getAccessToken()}`},
-    data: {
-      duration: this.state.counter,
-      clocktimerstampstop: `${moment().format('YYYY-MM-DD HH:mm:ss')}`
+  _onClick = (e) => {
+    if (!this.state.counting) {
+      this.startCounting()
     }
-  })
-//   axios.post(url, { headers: { Authorization: `Bearer ${getAccessToken()}`}})
-//   .then((data)=> {console.log(data)})
-}
-
-
-_onClick = (e) => {
-  if (!this.state.counting) {
-    this.startCounting()
+    if (this.state.counting) {
+      this.stopCounting()
+      this.stampandsend()
+    }
   }
-  if (this.state.counting) {
-    this.stopCounting()
-    this.stampandsend()
-  }
-}
 
   render() {
     let onClick = this._onClick
