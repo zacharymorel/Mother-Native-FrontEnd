0fb1672c0065ea960aff8f93ea0f86040d5dbf9a
Jonathan Bentley <jonconnorbentley@gmail.com>
2017-10-14 14:44:09 -0400

    Timer component is FULLY FUNCTIONAL (aside from refresh on stop)

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -8,22 +8,43 @@ import Counter from './Counter'
 export default class Timer extends Component {
   state = {
     contractions: [],
-    url: 'https://the-best-mom-app.herokuapp.com/api/mom/contraction'
+    url: 'https://the-best-mom-app.herokuapp.com/api/mom/contraction',
+    frequency: '',
+    duration: ''
   }
 
   _avgFreq = () => {
+    let array = this.state.contractions
     let sum = 0
     let dif = 0
-    if (this.state.contractions) {
-      this.state.contractions.map((contraction,i) => {
-        if ((i-1)<this.state.contractions.length) {
-          let a = moment(contraction.clocktimerstampstop)
-          let b = moment((this.state.contractions[i+1]).clocktimerstampstop)
-          dif = b.diff(a, 'minutes')
-        }
-        return (sum += dif)
-      })
-      return (sum/this.state.contractions.length)
+    let avg = 0
+    if (array) {
+      for (let i = 0; i < (array.length-1); i++) {
+        let a = moment((array[i]).clocktimerstampstop)
+        let b = moment((array[i+1]).clocktimerstampstop)
+        dif = b.diff(a, 'minutes')
+        sum += dif
+        console.log(dif);
+      }
+      avg = (sum/array.length).toString()
+      avg = avg.slice(0, (avg[(avg.indexOf('.'))+1]))
+      this.setState({frequency: avg})
+    } else {
+      return ''
+    }
+  }
+
+  _avgDur = () => {
+    let array = this.state.contractions
+    let sum = 0
+    let avg = 0
+    if (array) {
+      for (let i = 0; i < array.length; i++) {
+        sum += array[i].duration
+      }
+      let avg = (sum/array.length).toString()
+      avg = avg.slice(0, (avg[(avg.indexOf('.'))+1]))
+      this.setState({duration: avg})
     } else {
       return ''
     }
@@ -32,13 +53,22 @@ export default class Timer extends Component {
   _dropContractionLogs = () => {
     axios.delete(this.state.url, {headers: { Authorization: `Bearer ${getAccessToken()}`}})
     .then(res=>console.log(res))
+    .then(res => {this._updateStats()})
   }
 
-  componentDidMount() {
+  _updateStats = () => {
     axios.get(this.state.url, {headers: { Authorization: `Bearer ${getAccessToken()}`}})
     .then(res=> {
       this.setState({contractions: res.data})
     })
+    .then(data => {
+      this._avgDur()
+      this._avgFreq()
+    })
+  }
+
+  componentDidMount() {
+    this._updateStats()
   }
 
 
@@ -52,24 +82,15 @@ export default class Timer extends Component {
       }
       return(
         <li key={i} className="contraction">
-          <span className="timestamp">Contration: {dt}</span>
+          <span className="timestamp">Contraction: {dt}</span>
           <span className="duration">Duration: {dur()}</span>
         </li>
       )
     })
-    let frequency = () => {
-      let a = this.state.contractions[0]
-      a = moment(a.clocktimerstampstop)
-      let b = this.state.contractions[1]
-      b = moment(b.clocktimerstampstop)
-      let dif = b.diff(a,'minutes')
-      console.log(dif)
-      return dif
-    }
     return (
       <div className="timer">
-        <Counter/>
-        <p className="averages">{frequency}  |  Avg Duration</p>
+        <Counter click={this._updateStats}/>
+        <p className="averages">{this.state.frequency}  |  {this.state.duration}</p>
         <ul className="laps">
           {contractionlis}
         </ul>
