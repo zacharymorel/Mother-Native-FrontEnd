ab0c44678fdb46865409b3c0884c1f7e7eea9c21
Jonathan Bentley <jonconnorbentley@gmail.com>
2017-10-16 21:28:35 -0400

    accidentally broke with fn.js delete. fixed.

--- src/components/Timer/Timer.js
+++ src/components/Timer/Timer.js
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import axios from 'axios'
 import moment from 'moment'
 import {getAccessToken} from '../../utils/AuthService'
-import {averageFrequency} from './Fn'
 
 import Counter from './Counter'
 
